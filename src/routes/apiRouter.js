78dc9f888dbbf35d8b3a77510beedb63c5c85c16
Travis H <tphdevdrop@gmail.com>
2018-09-04 21:32:53 -0500

    Update apiRouter.js

--- src/routes/apiRouter.js
+++ src/routes/apiRouter.js
@@ -1,7 +1,8 @@
+// part.02.B.1 - import router 
 const Router = require('express').Router
 
+// part.02.B.2 - initialize router instance
 const apiRouter  = Router()
-
 
 apiRouter.get('/', (req, res)=>{
   res.json({
@@ -10,7 +11,11 @@ apiRouter.get('/', (req, res)=>{
   })
 })
 
+// part.02.B.3 - set up router to respond to GET requests
+//              (to api/vendors) ... do this for all 3 routes
 apiRouter.get('/vendors', (req, res)=>{
+  
+  // part.02.B.4 - send json `res.json(...)`
   res.json([
     {id: 1, name: 'Lionheart Beach',  year_established: 2008},
     {id: 2, name: 'SCHUNNE', year_established: 2013},
@@ -27,4 +32,6 @@ apiRouter.get('/products', (req, res)=>{
   ])
 })
 
+
+// part.02.B.5 - export router
 module.exports = apiRouter
